c3d5be381546a83a12fa8c61eee3f15aeae63ea3
billysistian <billy.sistianputra@gmail.com>
2024-12-16 17:03:19 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$("document").ready(function($){
+    var nav = $('#navbar');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            nav.addClass("active");
+        } else {
+            nav.removeClass("active");
+        }
+    });
+});
+
+$(".openbtn").click(function () {
+    $(this).toggleClass('active');
+});
+
+$(function() {
+    $(window).scrollTop($('#productservice').offset().top);
+});
+
+setTimeout(function(){
+    $('#loadingcontent').modal('show'); 
+}, 1000);
+
+function linkproduct($link){
+    return window.location.href = $link;
+}
